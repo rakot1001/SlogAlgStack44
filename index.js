7f0ba2dcadebdc488b75abb31099acc2dae9f7e3
Vlad <vladdp70@gmail.com>
2021-05-26 07:24:07 +0300

    Примеры алгоритмов

--- index.js
+++ index.js
@@ -1,54 +1,45 @@
-class ListItem {
-  constructor(v) {
-    this._value = v;
-    this.next = null;
-    this.prev = null;
-  }
-  /**
-   * 
-   */
-  get value() {
-    return this._value;
-  }
+const nums = [1, 242545, 233, 4, 55, 32, 6, 7, 5432, 567, 221, 65, 4];
 
-  set value(v) {
-    this._value = v;
-  }
-}
-
-class LinkedList {
-  constructor(...args) {
-    this.head = null;
-    this.tail = null;
-    for (const item of args) {
-      this.push(item);
+const linerSearch = (arr, value) => {
+  //O(n)
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === value) {
+      return i;
     }
   }
-  push(v) {
-    const newItem = new ListItem(v);
+  return -1;
+};
 
-    if (this.length === 0) {
-      this.head = newItem;
-      this.tail = newItem;
+const createMultTable = (limit) => {
+  //O(n^2)
+  const table = [];
+
+  for (let i = 1; i < limit; i++) {
+    for (let j = 1; j < limit; j++) {
+      table.push(`${i}*${j} = ${i * j}`);
+    }
+  }
+  return table;
+};
+
+const sortMass = [1, 3, 4, 6, 7, 8, 9, 10, 11, 40];
+
+const binarySearch = (arr, value) => {
+  //O(log n)
+  let start = 0;
+  let end = arr.length - 1;
+  let middle = Math.round((start + end) / 2);
+
+  while (true) {
+    if (arr[middle] === value) {
+      return middle;
+    }
+    if (arr[middle] > value) {
+      end = middle;
+      middle = Math.ceil((end + start) / 2);
     } else {
-      this.tail.next = newItem;
-      newItem.prev = this.tail;
-      this.tail = newItem;
+      start = middle;
+      middle = Math.floor((end + start) / 2);
     }
-
-    return ++this.length;
   }
-}
-
-const list = new LinkedList(1, true, 3, "Alsace", false, 6);
-
-/**
- * 
- */
-// getCount(){
-//   if(this.friends.length){
-//     return this.friends.reduce((result,friend)=> result+friend.getCount(),this.count);
-//   }
-//   return this.count;
-// }
-
+};
